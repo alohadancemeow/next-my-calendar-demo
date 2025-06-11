182923f4b262f4d42a2bcef84d84939893c23c11
Alohadancemeow <nawongsak@gmail.com>
2025-06-11 15:29:20 +0700

    Update package dependencies to include new libraries such as @hookform/resolvers, @radix-ui components, react-hook-form, and zod for enhanced form handling and UI elements. Refactor layout styles for improved responsiveness and consistency.

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -7,10 +7,10 @@ export default function PrivateLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <header className="flex py-2 border-b bg-card">
-        <nav className="font-medium flex items-center text-sm gap-6 container">
+        <nav className="font-medium flex items-center text-sm gap-6 container m-auto">
           <div className="flex items-center gap-2 font-semibold mr-auto">
             <CalendarRange className="size-6" />
-            <span className="sr-only md:not-sr-only">Calendor</span>
+            <span className="sr-only md:not-sr-only">Calendar</span>
           </div>
           <NavLink href="/events">Events</NavLink>
           <NavLink href="/schedule">Schedule</NavLink>
@@ -21,7 +21,7 @@ export default function PrivateLayout({ children }: { children: ReactNode }) {
           </div>
         </nav>
       </header>
-      <main className="container my-6">{children}</main>
+      <main className="container my-6 m-auto">{children}</main>
     </>
   );
 }
---
+++ server/actions/events.ts
@@ -0,0 +1,70 @@
+"use server";
+
+import { db } from "@/drizzle/db";
+import { EventTable } from "@/drizzle/schema";
+import { eventFormSchema } from "@/schema/events";
+import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+// Create event
+export async function createEvent(
+  unsafeData: z.infer<typeof eventFormSchema>
+): Promise<{ error: boolean } | undefined> {
+  const { userId } = await auth();
+  const { success, data } = eventFormSchema.safeParse(unsafeData);
+
+  if (!success || userId == null) {
+    return { error: true };
+  }
+
+  await db.insert(EventTable).values({ ...data, clerkUserId: userId });
+
+  redirect("/events");
+}
+
+// Update event
+export async function updateEvent(
+  id: string,
+  unsafeData: z.infer<typeof eventFormSchema>
+): Promise<{ error: boolean } | undefined> {
+  const { userId } = await auth();
+  const { success, data } = eventFormSchema.safeParse(unsafeData);
+
+  if (!success || userId == null) {
+    return { error: true };
+  }
+
+  const { rowCount } = await db
+    .update(EventTable)
+    .set({ ...data })
+    .where(and(eq(EventTable.id, id), eq(EventTable.clerkUserId, userId)));
+
+  if (rowCount === 0) {
+    return { error: true };
+  }
+
+  redirect("/events");
+}
+
+// Delete event
+export async function deleteEvent(
+  id: string
+): Promise<{ error: boolean } | undefined> {
+  const { userId } = await auth();
+
+  if (userId == null) {
+    return { error: true };
+  }
+
+  const { rowCount } = await db
+    .delete(EventTable)
+    .where(and(eq(EventTable.id, id), eq(EventTable.clerkUserId, userId)));
+
+  if (rowCount === 0) {
+    return { error: true };
+  }
+
+  redirect("/events");
+}
